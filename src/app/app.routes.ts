ba238b77f01f735eca7944e3cc5b02c8925e3ee3
NadaEid4 <nada.eid@eitesal.org>
2026-02-03 15:33:15 +0200

    Initial commit: Angular + Supabase project n

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Register } from './register/register';
 import { ReviewComponent } from './review/review';
 
 export const routes: Routes = [
+   { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'landing', component: Landing },
   { path: 'register', component: Register },
    { path: 'review', component: ReviewComponent },
